51827c081cc9b8804fd3cae3d0559e27de544c2e
alliumur <alliumur@live.com>
2020-03-19 21:39:37 +0100

    init

---
+++ src/main/java/pl/rmv/qwerty/QwertyApplication.java
@@ -0,0 +1,13 @@
+package pl.rmv.qwerty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rmv/qwerty/QwertyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rmv.qwerty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
