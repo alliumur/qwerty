8f02cfd46ff6ca73654c7d1ce36b00cca2197e58
alliumur <alliumur@live.com>
2020-04-08 22:55:21 +0200

    google recaptcha

--- src/main/java/pl/rmv/qwerty/configuration/McvConfig.java
+++ src/main/java/pl/rmv/qwerty/configuration/McvConfig.java
@@ -1,7 +1,9 @@
 package pl.rmv.qwerty.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +13,11 @@ public class McvConfig implements WebMvcConfigurer {
     //obsługa zasobów udostępnianych z serwera
     @Value("${upload.path}")
     private String uploadPath;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/pl/rmv/qwerty/controller/RegistrationController.java
+++ src/main/java/pl/rmv/qwerty/controller/RegistrationController.java
@@ -1,16 +1,19 @@
 package pl.rmv.qwerty.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 import pl.rmv.qwerty.model.Role;
 import pl.rmv.qwerty.model.User;
+import pl.rmv.qwerty.model.dto.CaptchaResponseDto;
 import pl.rmv.qwerty.repository.UserRepository;
 import pl.rmv.qwerty.service.UserService;
 import pl.rmv.qwerty.utils.ControllerUtils;
@@ -21,8 +24,16 @@ import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response";
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
 
     @GetMapping("/registration")
     private String registration(){
@@ -30,8 +41,14 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    private String registration(@Valid User user, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes){
-        if(userService.exist(user.getUsername())){
+    private String registration(@RequestParam("g-recaptcha-response") String captchaResponse, @Valid User user, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes){
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if(!response.isSuccess()){
+            model.addAttribute("messageType", "danger");
+            model.addAttribute("message", "Captcha nie uzupełniona");
+        }else if(userService.exist(user.getUsername())){
             bindingResult.rejectValue("username", "error.user", "Użytkownik istnieje z taką nazwą");
         }else if(user.getPassword() != null && !user.getPassword().equals(user.getPasswordConfirm())){
             bindingResult.rejectValue("passwordConfirm", "error.user", "Hasło różni się od poprzedniego");
